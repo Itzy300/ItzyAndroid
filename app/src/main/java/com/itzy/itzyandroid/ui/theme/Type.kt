a2db98976b883686da9b911ef26f3da9ea4087c6
WoeunJung <leena09123@naver.com>
2022-10-01 01:21:41 +0900

    feat: add font and typography setting

---
+++ app/src/main/java/com/itzy/itzyandroid/ui/theme/Type.kt
@@ -0,0 +1,45 @@
+package com.itzy.itzyandroid.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.itzy.itzyandroid.R
+
+private val Roboto = FontFamily(
+    Font(R.font.robotoblack, FontWeight.Black), // 900
+    Font(R.font.robotobold, FontWeight.Bold), // 700
+    Font(R.font.robotolight, FontWeight.Light), //300
+    Font(R.font.robotomedium, FontWeight.Medium), // 500
+    Font(R.font.robotoregular, FontWeight.Normal), // 400
+    Font(R.font.robotothin, FontWeight.Thin) // 100
+)
+
+object TextStyles{
+    val TextBasics1 = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+    )
+    val TextBasics2 = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+    )
+    val TextBasics3 = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+    )
+
+
+}
+val Typography = Typography(
+    body1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
+)
