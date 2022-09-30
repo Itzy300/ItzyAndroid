dc5f19877dbe6fa8176b7fc0b0a4713d0e2684e9
WoeunJung <leena09123@naver.com>
2022-10-01 02:59:03 +0900

    design: add colors

--- app/src/main/java/com/itzy/itzyandroid/ui/theme/Color.kt
+++ app/src/main/java/com/itzy/itzyandroid/ui/theme/Color.kt
@@ -3,5 +3,6 @@ package com.itzy.itzyandroid.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val MainBlue = Color(0xFF4A5AE1)
+val MainBlue2 = Color(0xFFF6F6FD)
 val MainGray1 = Color(0xFFF5F5F5)
-
+val MainGray2 = Color(0xFF7B7B7E)
