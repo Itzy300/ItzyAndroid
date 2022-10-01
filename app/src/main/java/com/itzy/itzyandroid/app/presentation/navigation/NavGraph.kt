753b6672e99249651aef6101f4d4d027ced02bac
WoeunJung <leena09123@naver.com>
2022-10-02 01:10:42 +0900

    feat: naviagting of DetailScreen

--- app/src/main/java/com/itzy/itzyandroid/app/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/itzy/itzyandroid/app/presentation/navigation/NavGraph.kt
@@ -4,6 +4,7 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.itzy.itzyandroid.app.presentation.view.HomeScreen
+import com.itzy.itzyandroid.app.presentation.view.PostDetailScreen
 import com.itzy.itzyandroid.app.presentation.view.PostScreen
 
 fun NavGraphBuilder.itzyGraph(
@@ -18,5 +19,10 @@ fun NavGraphBuilder.itzyGraph(
         route = Screen.PostScreen.route
     ) {
         PostScreen(navController)
+    }
+    composable(
+        route = Screen.PostDetailScreen.route
+    ) {
+        PostDetailScreen(navController)
     }
 }
--- app/src/main/java/com/itzy/itzyandroid/app/presentation/navigation/Screen.kt
+++ app/src/main/java/com/itzy/itzyandroid/app/presentation/navigation/Screen.kt
@@ -3,4 +3,5 @@ package com.itzy.itzyandroid.app.presentation.navigation
 sealed class Screen (val route: String){
     object HomeScreen: Screen("home_screen")
     object PostScreen: Screen("post_screen")
+    object PostDetailScreen: Screen("post_detail_screen")
 }
