d7762c22460f8655e2a768b58e71221ba84336bb
WoeunJung <leena09123@naver.com>
2022-10-02 01:09:05 +0900

    design: make component of DetailScreen

---
+++ app/src/main/java/com/itzy/itzyandroid/app/presentation/component/LikeComment.kt
@@ -0,0 +1,56 @@
+package com.itzy.itzyandroid.app.presentation.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.itzy.itzyandroid.R
+import com.itzy.itzyandroid.ui.theme.MainGray3
+import com.itzy.itzyandroid.ui.theme.TextStyles
+
+@Composable
+fun LikeComment(
+    modifier: Modifier = Modifier,
+    like: Int = 0,
+    comment: Int = 0,
+    size: Dp = 21.dp,
+){
+    Row(
+        modifier = modifier
+    ) {
+        Icon(
+            painterResource(id = R.drawable.like),
+            contentDescription = "like image",
+            modifier = Modifier.size(size),
+            tint = MainGray3
+        )
+        Text(
+            text = like.toString(),
+            style = TextStyles.TextSmall1,
+            color = MainGray3,
+            modifier = Modifier.align(CenterVertically).padding(start = 7.dp, end = 10.dp)
+        )
+        Icon(
+            painterResource(id = R.drawable.message),
+            contentDescription = "comment image",
+            modifier = Modifier.size(size),
+            tint = MainGray3
+        )
+        Text(
+            text = comment.toString(),
+            style = TextStyles.TextSmall1,
+            color = MainGray3,
+            modifier = Modifier.align(CenterVertically).padding(start = 7.dp, end = 10.dp)
+        )
+    }
+}
--- app/src/main/java/com/itzy/itzyandroid/app/presentation/component/PostItem.kt
+++ app/src/main/java/com/itzy/itzyandroid/app/presentation/component/PostItem.kt
@@ -5,10 +5,12 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.End
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.itzy.itzyandroid.R
@@ -29,10 +31,11 @@ fun PostItem(
             text = title,
             style = TextStyles.TextBasics2,
             color = Color.Black,
+            fontWeight = FontWeight.Bold,
             maxLines = 1,
             overflow = TextOverflow.Ellipsis
         )
-        Spacer(modifier = Modifier.height(10.dp))
+        Spacer(modifier = Modifier.height(13.dp))
         Text(
             text = content,
             style = TextStyles.TextSmall1,
@@ -40,33 +43,8 @@ fun PostItem(
             maxLines = 2,
             overflow = TextOverflow.Ellipsis
         )
-        Spacer(modifier = Modifier.height(5.dp))
-        Row(
-          modifier = Modifier.align(End)
-        ){
-            Image(
-                painterResource(id = R.drawable.like),
-                contentDescription = "like image",
-                modifier = Modifier.size(21.dp)
-            )
-            Text(
-                text = like.toString(),
-                style = TextStyles.TextSmall1,
-                color = MainGray3,
-                modifier = Modifier.padding(start = 7.dp, end = 10.dp)
-            )
-            Image(
-                painterResource(id = R.drawable.message),
-                contentDescription = "comment image",
-                modifier = Modifier.size(21.dp)
-            )
-            Text(
-                text = comment.toString(),
-                style = TextStyles.TextSmall1,
-                color = MainGray3,
-                modifier = Modifier.padding(start = 7.dp, end = 10.dp)
-            )
-        }
+        Spacer(modifier = Modifier.height(8.dp))
+        LikeComment(modifier = Modifier.align(End))
         Divider(
             modifier = Modifier
                 .padding(top = 7.dp)
