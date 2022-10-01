2552032590674121b966418e8dda5e261b7596a4
WoeunJung <leena09123@naver.com>
2022-10-01 11:43:24 +0900

    feat: edit LocationPost title

--- app/src/main/java/com/itzy/itzyandroid/app/_enums/LocationPost.kt
+++ app/src/main/java/com/itzy/itzyandroid/app/_enums/LocationPost.kt
@@ -1,14 +1,14 @@
 package com.itzy.itzyandroid.app._enums
 
 sealed class LocationPost(val tag: String, val name: String) {
-    object Seoul : LocationPost("Seoul", "서울인천경기")
-    object Busan: LocationPost("Busan", "부산울산경남")
-    object Daegu : LocationPost("Daegu", "대구경북")
-    object Chungbuk : LocationPost("Chungbuk", "충북")
-    object Gangwon : LocationPost("Gangwon", "강원")
-    object Chungnam : LocationPost("Chungnam", "충남")
-    object Jeonbuk  : LocationPost("Jeonbuk", "전북")
-    object Gwangju : LocationPost("Gwangju", "광주전남")
-    object Jeju : LocationPost("Jeju", "제주")
+    object Seoul : LocationPost("Seoul", "서울ㆍ인천ㆍ경기 게시판")
+    object Busan: LocationPost("Busan", "부산ㆍ울산ㆍ경남 게시판")
+    object Daegu : LocationPost("Daegu", "대구ㆍ경북 게시판")
+    object Chungbuk : LocationPost("Chungbuk", "충북 게시판")
+    object Gangwon : LocationPost("Gangwon", "강원 게시판")
+    object Chungnam : LocationPost("Chungnam", "충남 게시판")
+    object Jeonbuk  : LocationPost("Jeonbuk", "전북 게시판")
+    object Gwangju : LocationPost("Gwangju", "광주ㆍ전남 게시판")
+    object Jeju : LocationPost("Jeju", "제주 게시판")
 }
 
